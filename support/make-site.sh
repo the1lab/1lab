783b19920e7287b0ceb20bee35d8ee606c30053e
Amélia Liao <me@amelia.how>
2022-01-02 16:26:41 -0300

    wip: use absolute path

--- support/make-site.sh
+++ support/make-site.sh
@@ -2,5 +2,5 @@
 
 cp _build/{html,site} -rv
 mkdir -p _build/site/static/
-cp ~/static/* _build/site/static/ -rv
+cp /root/static/* _build/site/static/ -rv
 cp /lib/node_modules/katex/dist/{katex.min.css,fonts} _build/site/css -rv
