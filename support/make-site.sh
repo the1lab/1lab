d526fede4cd1e21de710779c50e9527ae15118d0
Amélia Liao <me@amelia.how>
2022-01-13 03:49:23 -0300

    fixup: Follow symlinks when copying KaTeX assets

--- support/make-site.sh
+++ support/make-site.sh
@@ -3,5 +3,5 @@
 cp _build/html _build/site -rv
 mkdir -p _build/site/static/ _build/site/css
 cp /root/static/* _build/site/static/ -rv
-cp /lib/node_modules/katex/dist/katex.min.css _build/site/css -rv
-cp /lib/node_modules/katex/dist/fonts _build/site/css -rv
+cp -L /lib/node_modules/katex/dist/katex.min.css _build/site/css -rv
+cp -L /lib/node_modules/katex/dist/fonts _build/site/css -rv
