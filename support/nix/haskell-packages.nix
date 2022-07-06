413ec59c7530c37c26e2c5961d865ea1cba78f8f
Jonathan Coates <git@squiddev.cc>
2022-07-06 16:04:19 +0100

    Update to latest Agda
    Deserialisation go brrrr

--- support/nix/haskell-packages.nix
+++ support/nix/haskell-packages.nix
@@ -1,11 +1,13 @@
 with import ./nixpkgs.nix;
 haskellPackages.override {
   overrides = self: super: {
-    Agda = (haskellPackages.callCabal2nix "Agda" (pkgs.fetchFromGitHub {
+    Agda = (self.callCabal2nix "Agda" (pkgs.fetchFromGitHub {
       owner = "agda";
       repo = "agda";
-      rev = "d36cfd1fbf88c49d2169fa5fafc762d64c985a72";
-      sha256 = sha256:0bkw3gh58kpixr0f7h60b0x3yzhinxbnzv9jmrzrdrivb9jvf7si;
+      rev = "41b2f45dbe83acda7e28d4e150ca8548f04b92fb";
+      sha256 = "sha256-jBvyp5jlwNAEtwe+n7kJp3cRib+n7r1vsBlTlJolhRs=";
     }) {}).overrideAttrs (old: { doCheck = false; });
+
+    vector-hashtables = haskell.lib.dontCheck super.vector-hashtables;
   };
 }
--- support/nix/nixpkgs.nix
+++ support/nix/nixpkgs.nix
@@ -2,4 +2,4 @@ import (builtins.fetchTarball {
   name = "1lab-nixpkgs-nixos-22.05";
   url = "https://github.com/nixos/nixpkgs/archive/7c1e79e294fe1be3cacb6408e3983bf2836c818e.tar.gz";
   sha256 = sha256:07p29mfxfw5820r4xak70mh5yijll5gkafv871519zrh6kqyr1wd;
-}) {}
+}) { config.allowBroken = true; }
