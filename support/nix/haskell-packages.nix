b47fa08063a4b40f705f9e7e1ce920a31a64a473
Naïm Favier <n@monade.li>
2022-08-18 18:39:59 +0200

    🐋 → ❄️ (#126)
    * chore: use Nix for CI
    
    * chore: bump nixpkgs
    
    Unbreaks vector-hashtables

--- support/nix/haskell-packages.nix
+++ support/nix/haskell-packages.nix
@@ -1,13 +1,12 @@
-with import ./nixpkgs.nix;
-haskellPackages.override {
-  overrides = self: super: {
-    Agda = (self.callCabal2nix "Agda" (pkgs.fetchFromGitHub {
-      owner = "agda";
-      repo = "agda";
-      rev = "a52fc3ca191b58e552626988b663bf76c6e8cc42";
-      sha256 = "sha256-pkaefBrZDr/1cP7G+uoCtyPDFprFCA6sixJdFNIvuqw=";
-    }) {}).overrideAttrs (old: { doCheck = false; });
-
-    vector-hashtables = haskell.lib.dontCheck super.vector-hashtables;
+pkgs: super: {
+  haskell = super.haskell // {
+    packageOverrides = hpkgs: hsuper: {
+      Agda = pkgs.haskell.lib.dontCheck (hpkgs.callCabal2nix "Agda" (pkgs.fetchFromGitHub {
+        owner = "agda";
+        repo = "agda";
+        rev = "a52fc3ca191b58e552626988b663bf76c6e8cc42";
+        sha256 = "sha256-pkaefBrZDr/1cP7G+uoCtyPDFprFCA6sixJdFNIvuqw=";
+      }) {});
+    };
   };
 }
