5884c2400659a0172968a9521fe7b0ab35e95d9d
Naïm Favier <n@monade.li>
2023-08-27 16:00:40 +0200

    chore: nix: allow building in pure mode

--- support/nix/nixpkgs.nix
+++ support/nix/nixpkgs.nix
@@ -1,7 +1,7 @@
-import (builtins.fetchTarball {
+args: import (builtins.fetchTarball {
   name   = "1lab-nixpkgs";
   url    = "https://github.com/nixos/nixpkgs/archive/cc4bb87f5457ba06af9ae57ee4328a49ce674b1b.tar.gz";
   sha256 = "sha256:072q50x5p06qjf0cvz68gcrbkpv94bdl55h71j0rz6bgfhaqmiwy";
-}) {
+}) ({
   overlays = [ (import ./haskell-packages.nix) ];
-}
+} // args)
