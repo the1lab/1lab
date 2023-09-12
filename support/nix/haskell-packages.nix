71cb456414a47e2ad1141266d90df9393aa8bc1c
Jonathan Coates <git@squiddev.cc>
2023-09-12 14:21:09 +0100

    Update nixpkgs (#257)
    This allows us to use the binary cache for everything but Agda. This
    allows us to use HLS without rebuilding everything (see #255).
    
    There has been a [breaking change to
    fsnotify](https://hackage.haskell.org/package/fsnotify-0.4.0.0/changelog),
    hence the slight change to the shakefile.

--- support/nix/haskell-packages.nix
+++ support/nix/haskell-packages.nix
@@ -14,49 +14,9 @@ in
   {
     # Can't just override all Haskell packages because callCabal2nix
     # somehow depends on mime-types
-    labHaskellPackages = super.haskell.packages.ghc943.override (old: {
+    labHaskellPackages = super.haskell.packages.ghc946.override (old: {
       overrides = self: super: {
         Agda = noProfile (noJunk (super.callCabal2nixWithOptions "Agda" (thunkSource ./dep/Agda) "-f optimise-heavily" {}));
-
-
-        # Currently broken (set to 0 so other packages which depend on
-        # it explode)
-        hspec-contrib         = 0;
-        hspec-core            = 0;
-
-        # Depend on hspec for their test suites. Just don't test them
-        JuicyPixels           = noJunk super.JuicyPixels;
-        doctest               = noJunk super.doctest;
-        shelly                = noJunk super.shelly;
-        hls-graph             = noJunk super.hls-graph;
-
-        pandoc                = noJunk (self.callCabal2nix "pandoc" (thunkSource ./dep/pandoc) {});
-        pandoc-types          = noJunk (self.callCabal2nix "pandoc-types" (thunkSource ./dep/pandoc-types) {});
-        commonmark            = noJunk (self.callCabal2nix "commonmark" ((thunkSource ./dep/commonmark-hs) + "/commonmark") {});
-        commonmark-extensions = noJunk (self.callCabal2nix "commonmark-extensions" ((thunkSource ./dep/commonmark-hs) + "/commonmark-extensions") {});
-        commonmark-pandoc     = noJunk (self.callCabal2nix "commonmark-pandoc" ((thunkSource ./dep/commonmark-hs) + "/commonmark-pandoc") {});
-        texmath               = noJunk (self.callCabal2nix "texmath" (thunkSource ./dep/texmath) {});
-        citeproc              = noJunk (self.callCabal2nix "citeproc" (thunkSource ./dep/citeproc) {});
-        doclayout             = noJunk (self.callCabal2nix "doclayout" (thunkSource ./dep/doclayout) {});
-        doctemplates          = noJunk (self.callCabal2nix "doctemplates" (thunkSource ./dep/doctemplates) {});
-        gridtables            = noJunk (self.callCabal2nix "gridtables" (thunkSource ./dep/gridtables) {});
-        jira-wiki-markup      = noJunk (self.callCabal2nix "jira-wiki-markup" (thunkSource ./dep/jira-wiki-markup) {});
-        mime-types            = noJunk (self.callCabal2nix "mime-types" ((thunkSource ./dep/wai) + "/mime-types") {});
-        skylighting           = noJunk (pkgs.haskell.lib.overrideCabal
-          (self.callCabal2nix "skylighting" ((thunkSource ./dep/skylighting) + "/skylighting") {})
-          {
-            preConfigure = ''
-            rm changelog.md
-            cp ${((thunkSource ./dep/skylighting) + "/changelog.md")} .
-            ${self.skylighting-core}/bin/skylighting-extract ${((thunkSource ./dep/skylighting) + "/skylighting-core/xml")}
-            '';
-          });
-        skylighting-format-ansi       = noJunk (self.callCabal2nix "skylighting-format-ansi" ((thunkSource ./dep/skylighting) + "/skylighting-format-ansi") {});
-        skylighting-format-blaze-html = noJunk (self.callCabal2nix "skylighting-format-blaze-html" ((thunkSource ./dep/skylighting) + "/skylighting-format-blaze-html") {});
-        skylighting-format-context    = noJunk (self.callCabal2nix "skylighting-format-context" ((thunkSource ./dep/skylighting) + "/skylighting-format-context") {});
-        skylighting-format-latex      = noJunk (self.callCabal2nix "skylighting-format-latex" ((thunkSource ./dep/skylighting) + "/skylighting-format-latex") {});
-        skylighting-core              = noJunk
-          (self.callCabal2nixWithOptions "skylighting-core" ((thunkSource ./dep/skylighting) + "/skylighting-core") "-fexecutable" {});
       };
     });
   }
