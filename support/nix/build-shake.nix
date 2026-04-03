2c864385ea607781347c7892595d7cbf90c2d5f5
Amélia Liao <me@amelia.how>
2026-04-03 14:01:39 -0300

    chore: bump agda

--- support/nix/build-shake.nix
+++ support/nix/build-shake.nix
@@ -11,19 +11,23 @@ let
   # To cut down on the image size we maim some references.
   forbiddenRefs = with labHaskellPackages; [
     shake tagsoup uri-encode
-    aeson Agda pandoc SHA pandoc-types HTTP
+    aeson pandoc SHA pandoc-types HTTP
     js-flot js-jquery js-dgtable
+
+    Agda.nodebug
   ];
 
   root = nix-gitignore.gitignoreSource [] ../shake;
 in
-lib.pipe (labHaskellPackages.callCabal2nix "1lab-shake" root {}) [
-  (hlib.addBuildTools [ removeReferencesTo ])
-  (hlib.overrideCabal (drv: {
-    postInstall = drv.postInstall or "" + ''
-      find "$out/bin" -exec remove-references-to ${lib.concatMapStringsSep " " (x: "-t ${lib.escapeShellArg x}") forbiddenRefs} '{}' +
-    '';
-    # On Darwin the references remain through symbolic links in /lib/links/*.dylib
-    disallowedRequisites = lib.optionals (!stdenv.hostPlatform.isDarwin) forbiddenRefs;
-  }))
-]
+  lib.pipe (labHaskellPackages.callCabal2nix "1lab-shake" root {
+    Agda = labHaskellPackages.Agda.nodebug;
+  }) [
+    (hlib.addBuildTools [ removeReferencesTo ])
+    (hlib.overrideCabal (drv: {
+      postInstall = drv.postInstall or "" + ''
+        find "$out/bin" -exec remove-references-to ${lib.concatMapStringsSep " " (x: "-t ${lib.escapeShellArg x}") forbiddenRefs} '{}' +
+      '';
+      # On Darwin the references remain through symbolic links in /lib/links/*.dylib
+      disallowedRequisites = lib.optionals (!stdenv.hostPlatform.isDarwin) forbiddenRefs;
+    }))
+  ]
