e6dfdb4b4b49057c94d2b9f9b20aab94eb96918d
Amélia Liao <me@amelia.how>
2026-06-26 11:07:15 -0300

    chore: bump

--- support/nix/haskell-packages.nix
+++ support/nix/haskell-packages.nix
@@ -16,8 +16,8 @@ let
 
   src = pkgs.fetchgit {
     url             = "https://codeberg.org/1lab/mikan.git";
-    rev             = "f4e25a2c2c84297337e3ee5d3b2e793390cbc3e5";
-    sha256          = "sha256-T6juKuSYuRS0SWY0pndMUD04IEYdcwHwAjUr4fzQMeg=";
+    rev             = "0f415fe6d1e9c5430bba3451ff93c7869041b1d3";
+    sha256          = "sha256-+z6zSVTfPWndQsy2zRjl2vmFLcvIGZ4ReDI059fhT+4=";
     fetchSubmodules = false;
   };
 
